a1686ebe4e0e6ed6afb00b30b07c501ee3828dff
Alessia Smeraglia <alessia.smeraglia@gmail.com>
2026-08-24 20:25:53 +0200

    aggiunto hook useDate

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,32 @@
 import useSwitch from "./hooks/useSwitch";
+import useDate from "./hooks/useDate";
 
 function App() {
   const [isOn, toggle] = useSwitch();
+  const currentDate = useDate();
 
   return (
     <main className="container">
-      <h1>Snack 1: useSwitch</h1>
 
-      <p className={isOn ? 'status on' : 'status off'}>
-        Il valore è: {isOn ? 'ON' : 'OFF'}
-      </p>
+      <section className="card">
+        <h1>Snack 1: useSwitch</h1>
 
-      <button onClick={toggle}>
-        Cambia stato
-      </button>
+        <p className={isOn ? 'status on' : 'status off'}>
+          Il valore è: {isOn ? 'ON' : 'OFF'}
+        </p>
+
+        <button onClick={toggle}>
+          Cambia stato
+        </button>
+      </section>
+
+      <section className="card">
+        <h1>Snack 2: useDate</h1>
+
+        <p className="date">
+          {currentDate.toLocaleString('it-IT')}
+        </p>
+      </section>
     </main>
   );
 }
