7576bae592b2bcb5bb1114313dd3b886b3f712ef
Alessia Smeraglia <alessia.smeraglia@gmail.com>
2026-08-24 20:16:56 +0200

    Aggiunto hook useSwitch

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import { useState } from 'react'
+import useSwitch from "./hooks/useSwitch";
 
 function App() {
+  const [isOn, toggle] = useSwitch();
 
   return (
-    <>
-      
-    </>
-  )
+    <main className="container">
+      <h1>Snack 1: useSwitch</h1>
+
+      <p className={isOn ? 'status on' : 'status off'}>
+        Il valore è: {isOn ? 'ON' : 'OFF'}
+      </p>
+
+      <button onClick={toggle}>
+        Cambia stato
+      </button>
+    </main>
+  );
 }
 
 export default App
